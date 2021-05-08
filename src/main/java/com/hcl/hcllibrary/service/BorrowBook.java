6d11259bdf8e186ef406ecd8e6c6ba6d1b9800a8
Harsha Gedda <geddaharsha@gmail.com>
2021-05-08 13:29:08 -0500

    add service classes

---
+++ src/main/java/com/hcl/hcllibrary/service/BorrowBook.java
@@ -0,0 +1,11 @@
+package com.hcl.hcllibrary.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BorrowBook {
+	
+	public Object borrowBook() {
+		return null;
+	}
+}
---
+++ src/main/java/com/hcl/hcllibrary/service/SearchBook.java
@@ -0,0 +1,11 @@
+package com.hcl.hcllibrary.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchBook {
+
+	public Object searchBook(String title) {
+		return null;
+	}
+}
