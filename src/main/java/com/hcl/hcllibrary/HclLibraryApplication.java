d7f131bc4f414786ffcfecdad34749dd11823e8e
Harsha Gedda <geddaharsha@gmail.com>
2021-05-08 11:37:08 -0500

    spring boot project initial

---
+++ src/main/java/com/hcl/hcllibrary/HclLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.hcllibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/hcllibrary/HclLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hcllibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
